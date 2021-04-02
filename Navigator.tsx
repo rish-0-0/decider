f79fe195448677b208a2a568c313f00eb2674ce3
Rishabh Anand <superuser@Rishabhs-MacBook-Air.local>
2021-04-02 16:57:33 +0530

    added icons to the tab navigator 	modified:   Navigator.tsx 	modified:   ios/Decider.xcodeproj/project.pbxproj 	modified:   ios/Podfile.lock 	modified:   package.json 	modified:   yarn.lock

--- Navigator.tsx
+++ Navigator.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FeatherIcons from 'react-native-vector-icons/Feather';
 
 import People from './Screens/People';
 import Playground from './Screens/Playground';
@@ -7,18 +9,48 @@ import History from './Screens/History';
 
 const Tabs = createBottomTabNavigator();
 
-interface NavigationProps {
+interface NavigationProps {}
 
-}
-
-const AppNavigation: React.FC<NavigationProps> = (props) => {
+const AppNavigation: React.FC<NavigationProps> = props => {
   return (
-    <Tabs.Navigator>
-      <Tabs.Screen name="Playground" component={Playground} />
-      <Tabs.Screen name="People" component={People} />
-      <Tabs.Screen name="History" component={History} />
+    <Tabs.Navigator initialRouteName="Playground">
+      <Tabs.Screen
+        name="Playground"
+        component={Playground}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="racquetball"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="People"
+        component={People}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <FeatherIcons name="users" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="History"
+        component={History}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="history"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
     </Tabs.Navigator>
   );
-}
+};
 
 export default AppNavigation;
